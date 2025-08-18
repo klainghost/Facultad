46cc17804f71b5672bffadec985ce638b5dfeb27
Gabriel Moya - HP Pavilion - WIN <gabymoya12@gmail.com>
2025-08-18 19:35:22 -0300

    Agrego carpeta TUP solo para la UTN

---
+++ TUP/Programación II/PYTHON/Clase 1.py
@@ -0,0 +1,18 @@
+# Ejercicio 1: Iterar un rango de 0 a 10 e imprimir números divisibles entre 3
+# Ejemplo de ejecución: 0,3,6,9
+print ("Ejercicio 1")
+for x in range (0,11):
+    if x % 3 ==0:
+        print (x)
+
+# Ejercicio 2: Crear un rango de numeros de 2 a 6 a imprimirlos
+# Ejemplo de ejecución: 2,3,4,5,6
+print ("Ejercicio 2")
+for y in range (2,7):
+    print (y)
+# Ejercicio 3: Crear un rango de 3 a 10 pero con incremento de 2 en 2, en lugar de 1 en 1
+#Ejemplo de ejecución: 3,5,7,9
+print ("Ejercicio 3")
+for z in range (3,11,2):
+    print (z)
+
